4828ebe11a4f6deae4bc30a4d0e855ddd523db1e
Kabsius <47015320+Kabsius@users.noreply.github.com>
2019-03-05 20:39:11 -0500

    Add files via upload

---
+++ Practica PAPW/js/mio.js
@@ -0,0 +1,25 @@
+
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
+
+
+window.onload = function() {
+	  document.getElementById("leftMenu").style.display = "block";
+	  document.getElementById("leftMenu").style.display = "none";
+	    document.getElementById("rightMenu").style.display = "block";
+	      document.getElementById("rightMenu").style.display = "none";
+
+}
